eb1e85a72dd50f9f9401868aebc6b7a3d11a5daf
Romain Pellerin <rpellerin@users.noreply.github.com>
2021-10-07 23:55:22 +0200

    WIP

---
+++ src/Activities.js
@@ -0,0 +1,96 @@
+import { useEffect, useState } from "react";
+import * as geolib from "geolib";
+
+const isCommuteRide = (
+  activity,
+  days = [1, 2, 3, 4, 5],
+  zones = [{ lat: 52.491615, lng: 13.387418 }]
+) => {
+  const startDate = new Date(activity.start_date);
+  const isBikeRide = activity.type === "Ride";
+  const matchesDays = days.includes(startDate.getUTCDay());
+  const isInZones = zones.some(
+    (zone) =>
+      (activity.start_latlng.length > 0 &&
+        geolib.isPointWithinRadius(
+          { latitude: zone.lat, longitude: zone.lng },
+          {
+            latitude: activity.start_latlng[0],
+            longitude: activity.start_latlng[1],
+          },
+          250 // 250 meters
+        )) ||
+      (activity.end_latlng.length > 0 &&
+        geolib.isPointWithinRadius(
+          { latitude: zone.lat, longitude: zone.lng },
+          {
+            latitude: activity.end_latlng[0],
+            longitude: activity.end_latlng[1],
+          },
+          250
+        ))
+  );
+  return matchesDays && isBikeRide && isInZones;
+};
+
+const CommuteOrNotTag = ({ activity }) => {
+  const isCommute = isCommuteRide(activity);
+  const children = isCommute ? "COMMUTE" : "Not a commute";
+
+  const style = {
+    padding: "10px",
+    borderRadius: "5px",
+    backgroundColor: "orange",
+  };
+  if (isCommute) {
+    style.backgroundColor = "green";
+  }
+  return <span style={style}>{children}</span>;
+};
+
+const Activities = ({ accessToken }) => {
+  const [activities, setActivities] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      "https://www.strava.com/api/v3/athlete/activities?per_page=100&page=1",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Activities", data);
+        setActivities(data.filter((activity) => !activity.commute));
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return loading ? (
+    <div>Loading ...</div>
+  ) : (
+    <ul>
+      {activities.map((activity) => {
+        const startDate = new Date(activity.start_date);
+        return (
+          <li key={activity.id}>
+            <h2>{activity.name}</h2>
+            <div>
+              Day of week:{" "}
+              {startDate.toLocaleDateString(undefined, { weekday: "long" })}{" "}
+              <CommuteOrNotTag activity={activity} />
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Activities;
--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import Activities from "./Activities";
+import "./App.css";
+
+const Login = () => {
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams?.get("code");
+    if (code) {
+      const data = {
+        client_id: 72614,
+        client_secret: process.env.REACT_APP_CLIENT_SECRET,
+        code,
+        grant_type: "authorization_code",
+      };
+      fetch("https://www.strava.com/oauth/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.errors) throw new Error(data.message);
+          console.log("Success:", data);
+          window.localStorage.setItem("userId", data.athlete.id);
+          window.localStorage.setItem("refreshToken", data.refresh_token);
+          window.localStorage.setItem("accessToken", data.access_token);
+          window.localStorage.setItem("accessTokenExpiresAt", data.expires_at);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  }, []);
+
+  return (
+    <a href="https://www.strava.com/oauth/authorize?client_id=72614&response_type=code&redirect_uri=http://localhost:3000/exchange_token&approval_prompt=auto&scope=read,activity:read_all,activity:write">
+      Login with Strava
+    </a>
+  );
+};
+
+const useCurrentUser = () => {
+  const accessToken = window.localStorage.getItem("accessToken");
+  const accessTokenExpiresAt = window.localStorage.getItem(
+    "accessTokenExpiresAt"
+  );
+  const isLoggedIn =
+    accessToken &&
+    accessTokenExpiresAt &&
+    new Date(parseInt(accessTokenExpiresAt, 10) * 1000) > new Date();
+  return { isLoggedIn, currentUser: { accessToken } };
+};
 
 function App() {
+  const { isLoggedIn, currentUser } = useCurrentUser();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Commute Auto Tagger</h1>
+        <main>
+          {isLoggedIn ? (
+            <Activities accessToken={currentUser.accessToken} />
+          ) : (
+            <Login />
+          )}
+        </main>
       </header>
     </div>
   );
