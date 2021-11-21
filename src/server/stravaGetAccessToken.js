d36664260b4517d4eb07697b0e1dae0b7eed5cfe
Juliette Hoisnard <juliette@hoisnard.fr>
2021-11-21 23:53:53 +0100

    add backend

---
+++ src/server/stravaGetAccessToken.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const httpsOptions = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const post = async (url, body) =>
+  new Promise((resolve, reject) => {
+    const req = https.request(url, httpsOptions, (res) => {
+      if (res.statusCode < 200 || res.statusCode > 299) {
+        return reject(new Error(`HTTP status code ${res.statusCode}`));
+      }
+
+      const body = [];
+      res.on("data", (chunk) => body.push(chunk));
+      res.on("end", () => {
+        const resString = Buffer.concat(body).toString();
+        resolve(resString);
+      });
+    });
+
+    req.on("error", (err) => reject(err));
+
+    req.on("timeout", () => {
+      req.destroy();
+      reject(new Error("Request time out"));
+    });
+
+    req.write(JSON.stringify(body));
+    req.end();
+  });
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+
+app.use(cors()); // Allow all origin. Remove in production.
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+const client_secret = process.env.CLIENT_SECRET;
+
+app.post("/strava-get-access-token", async (req, res) => {
+  try {
+    const responseFromStrava = await post(
+      "https://www.strava.com/api/v3/oauth/token",
+      { ...req.body, client_secret }
+    );
+    res.send(responseFromStrava);
+  } catch (e) {
+    res.send(`Error: ${e.message}`);
+  }
+});
