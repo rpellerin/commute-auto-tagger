417d2e83c44a223e8b04a3c46d4a51f7ea34d9d9
Romain Pellerin <rpellerin@users.noreply.github.com>
2021-12-21 13:15:43 +0100

    Add more tests

--- src/services/activity.test.js
+++ src/services/activity.test.js
@@ -9,6 +9,20 @@ describe("isPotentialCommuteRide", () => {
     it("returns false when the ride is not a bike ride", () => {
       expect(isPotentialCommuteRide({ type: "Walking" })).toBe(false);
     });
+
+    it("returns false when the days do not match", () => {
+      expect(
+        isPotentialCommuteRide(
+          {
+            type: "Ride",
+            start_latlng: [51, 20],
+            start_date: "2021-12-21 09:00",
+          },
+          [{ lat: 51, lng: 20, radius: 1 }],
+          [0]
+        )
+      ).toBe(false);
+    });
   });
 
   describe("when the ride is a potential commute ride", () => {
