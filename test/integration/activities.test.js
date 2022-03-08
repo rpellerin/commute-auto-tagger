900f17c1df3649fd8142e0b85b3d0bea4bb39df2
Juliette Hoisnard <juho@netlight.com>
2022-03-08 23:56:35 +0100

    Add test

---
+++ test/integration/activities.test.js
@@ -0,0 +1,25 @@
+const { test, expect } = require("@playwright/test");
+const { assertHeaderPresent, login, visitLandingPage } = require("./helpers/base");
+const { mockStravaGetAccessToken, mockStravaApiAthlete, mockStravaApiActivities } = require("./helpers/RequestMocker");
+
+test.describe("Activities", () => {
+  test.beforeEach(async ({ page }) => {
+    await mockStravaGetAccessToken(page);
+    await mockStravaApiAthlete(page);
+    await mockStravaApiActivities(page);
+    await login(page);
+    await assertHeaderPresent(page);
+  });
+
+  test.afterEach(async ({ page }) => {
+    await page.evaluate(() => localStorage.clear());
+  });
+
+  test("should display the activities of the user which are only of type 'Ride'", async ({
+    page,
+  }) => {
+    const locator = page.locator('ul li h2');
+    await expect(locator).toHaveCount(1);
+    await expect(locator).toHaveText('Bike ride');
+  });
+});
